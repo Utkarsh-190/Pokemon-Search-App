c59f36a61c19cbf144bc780e856b2d5c1743d07c
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-11-19 18:48:59 +0530

    changed routes

--- src/components/main/ListItem.js
+++ src/components/main/ListItem.js
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 
 function ListItem({ pokemon }) {
   return (
-    <Link to={`/details/${pokemon.id}`} style={{ textDecoration: "none" }}>
+    <Link
+      to={`/Pokemon-Search-App/details/${pokemon.id}`}
+      style={{ textDecoration: "none" }}
+    >
       <div className={classes.listItem}>
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
